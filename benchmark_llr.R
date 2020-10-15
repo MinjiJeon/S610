93bdb7b3f321537027c80ecbf6b7bfa0d1e80bbf
Minji <camillejeon@gmail.com>
2020-10-15 15:19:41 -0400

    initial commit

---
+++ benchmark_llr.R
@@ -0,0 +1,7 @@
+# Add code to the benchmark_file that computes how long your llr function takes to run and prints it out.
+
+# hint: use the function cat and one of bench::mark or microbenchmark::microbenchmark)
+
+cat()
+library(microbenchmark)
+microbenchmark
